cf833829a2c811552fba9bb0749a2ba4409d0c4e
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-13 09:02:04 -0500

    feat: mostrar lista http

--- lib/screens/vista_principal.dart
+++ lib/screens/vista_principal.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class VistaPrincipal extends StatelessWidget {
   const VistaPrincipal({super.key});
@@ -13,9 +16,44 @@ class VistaPrincipal extends StatelessWidget {
           icon: Icon(Icons.info),
         ),
       ),
-      body: Image.asset("assets/images/tmnt.jpg"),
+      body: Column(
+        children: [
+          Image.asset("assets/images/tmnt.jpg"),
+          Expanded(child: lista2(context)),
+        ],
+      ),
     );
   }
+}
+
+Future<List> leerJuegos(String url) async {
+  final peticion = await http.get(Uri.parse(url));
+  return jsonDecode(peticion.body)['videojuegos'];
+}
+
+Widget lista2(context) {
+  String url = "https://jritsqmet.github.io/web-api/videojuegos.json";
+  return (FutureBuilder(
+    future: leerJuegos(url),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        final data = snapshot.data!;
+        return (ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final item = data[index];
+            return (ListTile(
+              title: Text('${item['titulo']}'),
+              subtitle: Text('Precio: ${item['precio']}'),
+              leading: Image.network('${item['imagen']}'),
+            ));
+          },
+        ));
+      } else {
+        return (CircularProgressIndicator());
+      }
+    },
+  ));
 }
 
 void mostrarInfo(BuildContext context) {
