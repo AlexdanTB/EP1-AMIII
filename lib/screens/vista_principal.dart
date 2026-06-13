19f33b3ff142dd40abe7c314d901729f6af6487c
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-13 07:36:36 -0500

    feat: bottomtab

--- lib/screens/vista_principal.dart
+++ lib/screens/vista_principal.dart
@@ -10,6 +10,21 @@ class VistaPrincipal extends StatefulWidget {
 class _VistaPrincipalState extends State<VistaPrincipal> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Vista Principal')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Vista Principal')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Principal'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate_outlined),
+            label: 'Suero',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate_rounded),
+            label: 'Presion',
+          ),
+        ],
+      ),
+    );
   }
 }
