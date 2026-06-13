fd12a312cfa82c032e6cca23f7745cbcb7587c5a
AlexdanTB <alexdanieltb220@gmail.com>
2026-06-13 08:44:01 -0500

    feat: mostrar info

--- lib/screens/vista_principal.dart
+++ lib/screens/vista_principal.dart
@@ -6,8 +6,26 @@ class VistaPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Vista Principal')),
+      appBar: AppBar(
+        title: Text('Vista Principal'),
+        leading: IconButton(
+          onPressed: () => mostrarInfo(context),
+          icon: Icon(Icons.info),
+        ),
+      ),
       body: Image.asset("assets/images/tmnt.jpg"),
     );
   }
+}
+
+void mostrarInfo(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Ejercicios de Programación'),
+      content: Text(
+        'Programador: AlexdanTB \n Carrera: Desarrollo de Software',
+      ),
+    ),
+  );
 }
